0a74dd37c544d792deb64e50dbd2e74587ebc6d9
Taufiq Fharhan <m.taufiq.fharhan@gmail.com>
2024-12-22 23:37:27 +0800

    Added local font and configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      poppinsReg: "var(--font-poppins-reg)",
+    },
     screens: {
       sm: "375px",
       md: "768px",
       lg: "1200px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+      },
     },
     extend: {
       colors: {
