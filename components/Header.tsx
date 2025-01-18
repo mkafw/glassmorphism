ea74fc867c685101f66a25776c60609a765e6271
Taufiq Fharhan <m.taufiq.fharhan@gmail.com>
2025-01-19 00:42:16 +0800

    Updated section header to show black font instead of white

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,9 @@ const SectionHeader = ({
 }) => {
   return (
     <div className="relative">
-      <h2 className="text-3xl md:text-5xl text-center mt-6">{title}</h2>
+      <h2 className="text-3xl md:text-5xl text-center mt-6 text-black">
+        {title}
+      </h2>
       <p className="text-center md:text-lg lg:text-xl text-black/60 mt-4 max-w-2xl mx-auto">
         {description}
       </p>
